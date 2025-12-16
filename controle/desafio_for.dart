f6cec5eb1941228fa4639a911fb6fbb68f9fded8
Dênes Antonio <denesantonio97@gmail.com>
2025-12-16 16:40:27 -0300

    commit dia 16/12/2025 trabalho

---
+++ controle/desafio_for.dart
@@ -0,0 +1,14 @@
+// main() {
+//   var iterator = ['#', '#', '#', '#', '#', '#'];
+
+//   for (var i in iterator) {
+//     print(i);
+//     i += i;
+//   }
+// }
+
+main() {
+  for (var valor = '#'; valor != '#######'; valor += '#') {
+    print(valor);
+  }
+}
